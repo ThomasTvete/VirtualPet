f85b0ed34cd38dbdfce9efca72ca6f748bb6b4f1
Thomas <tgtvete@ntebb.no>
2024-05-24 14:37:32 +0200

    Add project files.

---
+++ VirtualPet/Admin.cs
@@ -0,0 +1,86 @@
+﻿namespace VirtualPet
+{
+    internal class Admin
+    {
+        private List<Pet> _pets = new List<Pet>()
+        {
+            new("Nico", false, true, true),
+            new("Bianca", true, false, false),
+            new("Loki", false,false,false),
+        };
+
+        public void RunVirtualPetProgram()
+        {
+            bool runningMain = true;
+            while (runningMain)
+            {
+                Console.Clear();
+                PrintTitle();
+                Console.WriteLine("Welcome to Virtual Pet!");
+                Console.WriteLine("Which pet would you like to take care of?");
+                RunShowPets();
+                Pet pickedPet = RunPickPet();
+                Console.WriteLine($"You picked: {pickedPet.Name}");
+                Console.WriteLine();
+                pickedPet.RunShowPetCareOptions();
+            }
+        }
+
+        public void PrintTitle()
+        {
+            Console.WriteLine(@" _    ___      __              __   ____       __ 
+| |  / (_)____/ /___  ______ _/ /  / __ \___  / /_
+| | / / / ___/ __/ / / / __ `/ /  / /_/ / _ \/ __/
+| |/ / / /  / /_/ /_/ / /_/ / /  / ____/  __/ /_  
+|___/_/_/   \__/\__,_/\__,_/_/  /_/    \___/\__/");
+        }
+
+        private void RunShowPets()
+        {
+            for (int i = 0; i < _pets.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {_pets[i].Name}");
+            }
+            Console.WriteLine("If you would like to make your own pet, tap 0.");
+            
+        }
+
+        private void RunMakePet()
+        {
+            Console.WriteLine("Name your pet");
+            string? petName = Console.ReadLine();
+            Pet newPet = new Pet (petName);
+            _pets.Add(newPet);
+            Console.WriteLine("\n");
+            RunShowPets();
+        }
+
+        private Pet RunPickPet()
+        {
+            bool runningPickPet = true;
+            int input = 0;
+            while (runningPickPet)
+            {
+                Console.WriteLine("Please type the number of the pet:");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input > _pets.Count || input < 0)
+                {
+                    Console.WriteLine("Yooo");
+                    
+                }
+                else if (input == 0)
+                {
+                    RunMakePet();
+                }
+                else { runningPickPet = false; }
+
+
+            } ;
+
+
+            return _pets.Where(p => p.Name == _pets[input - 1].Name).FirstOrDefault();
+        }
+
+
+    }
+}
---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,176 @@
+﻿
+namespace VirtualPet
+{
+    internal class Pet()
+    {
+        public string Name { get; private set; }
+        public bool IsHungry { get; private set; }
+        public bool IsPoopy { get; private set; }
+        public bool IsCuddly { get; private set; }
+
+        public bool running = true;
+
+        public bool isDead = false;
+
+        public Pet(string name) : this()
+        {
+            Name = name;
+            IsHungry = true;
+            IsPoopy = false;
+            IsCuddly = true;
+        }
+
+        public Pet(string name, bool isHungry, bool isPoopy, bool isCuddly) : this()
+        {
+            Name = name;
+            IsHungry = isHungry;
+            IsPoopy = isPoopy;
+            IsCuddly = isCuddly;
+        }
+
+        Random random = new Random();
+        
+
+        public void RunShowPetCareOptions()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pick an option:");
+            running = true;
+            while (running)
+            {
+                Console.WriteLine($"1. Feed {Name}");
+                Console.WriteLine($"2. Potty {Name}");
+                Console.WriteLine($"3. Cuddle {Name}");
+                Console.WriteLine("4. Pick another pet");
+                Console.WriteLine("5. Exit game");
+                int input = Convert.ToInt32(Console.ReadLine());
+                RunPickedOption(input);
+                
+                
+            }
+        }
+
+        public void RunPickedOption(int input)
+        {
+            switch (input)
+            {
+                case 1:
+                    RunOption1();
+                    break;
+                case 2:
+                    RunOption2();
+                    break;
+                case 3:
+                    RunOption3();
+                    break;
+                case 4:
+                    running = false;
+                    break;
+                case 5:
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Console.WriteLine("Please pick a valid option"); RunShowPetCareOptions();
+                    break;
+            }
+        }
+
+        public void RunOption1()
+        {
+            IsPoopy = RandomBool();
+            IsCuddly = RandomBool();
+
+            if (IsHungry)
+            {
+                Console.WriteLine($"You fed {Name}");
+                DoesItDie();
+                if(isDead) DeathMessage(1);
+                
+                IsHungry = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} was not hungry");
+                
+            }
+            RunShowPetCareOptions();
+        }
+        public void RunOption2()
+        {
+            IsHungry = RandomBool();
+            IsCuddly = RandomBool();
+
+            if (IsPoopy)
+            {
+                Console.WriteLine($"You took {Name} to use the bathroom");
+                DoesItDie();
+                if (isDead) DeathMessage(2);
+                IsPoopy = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} did not need to use the bathroom");
+                
+            }
+            RunShowPetCareOptions();
+        }
+        public void RunOption3()
+        {
+            IsHungry = RandomBool();
+            IsPoopy = RandomBool();
+
+            if (IsCuddly)
+            {
+                Console.WriteLine($"You cuddled {Name}");
+                DoesItDie();
+                if (isDead) DeathMessage(3);
+                IsCuddly = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} did not want to be cuddled");
+                
+            }
+            RunShowPetCareOptions();
+        }
+
+        public bool RandomBool()
+        {
+            int randomInt = random.Next(0, 2);
+            bool randomBool = randomInt == 0 ? true : false;
+
+            return randomBool;
+        }
+
+        public void DoesItDie()
+        {
+            int deathChance = random.Next(0, 60);
+            if (deathChance == 0)
+            {
+                isDead = true;
+            }
+        }
+
+        public void DeathMessage(int input)
+        {
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Your pet died of food poisoning");
+                    Environment.Exit(2);
+                    break;
+                case 2:
+                    Console.WriteLine("Your died of violent diarrhea");
+                    Environment.Exit(3);
+                    break;
+                case 3:
+                    Console.WriteLine("You squished your pet to death");
+                    Environment.Exit(4);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
